b5ac0759ef37ad1b6a0e009aa2433409347d9568
Blooker <evangriffiths@hotmail.co.uk>
2016-06-18 23:24:07 +0100

    Blocks can be placed, ghost block snaps to sides

---
+++ UnityFiles/BlockSnapTest/Assets/Scripts/GhostBlock.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostBlock : MonoBehaviour {
+
+    public GameObject block;
+    public bool followMouse = true;
+
+    SpriteRenderer spriteRender;
+    Vector3 mousePos;
+    Vector3 snapPos;
+
+	// Use this for initialization
+	void Start () {
+        spriteRender = GetComponent<SpriteRenderer>();
+        spriteRender.color = new Color(1f, 1f, 1f, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (followMouse) {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            //transform.position = Vector2.Lerp(transform.position, mousePos, 5);
+            transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(block, transform.position, transform.rotation);
+        }
+    }
+
+    public void SnapToPivot(Transform snapObject, Vector3 snapOffset, Vector3 pivotPos, Quaternion pivotRot) {
+        spriteRender.color = new Color(0f, 1f, 0f, 0.5f);
+
+        transform.parent = snapObject.parent.parent;
+        followMouse = false;
+        snapPos = pivotPos + snapOffset;
+
+        transform.localPosition = snapPos;
+        transform.rotation = pivotRot;
+    }
+
+    public void UnsnapFromPivot () {
+        spriteRender.color = new Color(1f, 1f, 1f, 0.5f);
+
+        followMouse = true;
+        transform.parent = null;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
---
+++ UnityFiles/BlockSnapTest/Assets/Scripts/SnapPoint.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnapPoint : MonoBehaviour {
+
+    public float offsetAmountX;
+    public float offsetAmountY;
+    public string triggerMessage;
+
+    GameObject ghostBlockGO;
+    GhostBlock ghostBlockScript;
+    Vector3 snapOffset;
+
+    void Start () {
+        ghostBlockGO = GameObject.Find("GhostBlock");
+        ghostBlockScript = ghostBlockGO.GetComponent<GhostBlock>();
+    }
+
+    void OnMouseEnter () {
+        //Debug.Log(triggerMessage);
+        snapOffset = new Vector3(offsetAmountX, offsetAmountY, 0);
+
+        ghostBlockScript.SnapToPivot(this.gameObject.transform, snapOffset, transform.localPosition, transform.rotation);
+    }
+
+    void OnMouseExit () {
+        ghostBlockScript.UnsnapFromPivot();
+    }
+}
