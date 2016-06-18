893e64899bc15b140dd30a09fb0ed68d9e7e526b
Blooker <evangriffiths@hotmail.co.uk>
2016-06-18 16:19:17 +0100

    Added concept art GIMP file, block test project

---
+++ BlockSnapTest/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+
+    public bool followMouse = true;
+
+    SpriteRenderer spriteRender;
+    Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+        spriteRender = GetComponent<SpriteRenderer>();
+        spriteRender.color = new Color(1f, 1f, 1f, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (followMouse) {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, 1);
+        }
+    }
+}
